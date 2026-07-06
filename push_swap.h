eaf46b8a3ea4c7ac6d722e210f0fd8e9a1e33de6
Henrique Santana Frameschi <hesantan@c1r3p2.42sp.org.br>
2026-07-06 15:28:48 -0300

    header

---
+++ push_swap.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hesantan <hesantan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/06 12:35:56 by tjulya-c          #+#    #+#             */
+/*   Updated: 2026/07/06 15:28:02 by hesantan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
+
+typedef struct s_stack
+{
+	int		size;
+	t_node	*top;
+	t_node	*bottom;
+}	t_stack;
+
+typedef struct s_bench
+{
+	
+}	t_bench;
+
+#endif
